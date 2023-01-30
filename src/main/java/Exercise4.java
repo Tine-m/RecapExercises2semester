6fc4d123d90dce54c9e54e4ed5dfbb566e05c812
Tine-m <tinemarbjerg@gmail.com>
2023-01-30 14:05:44 +0100

    Exercises specified via unit tests.

--- src/main/java/Exercise4.java
+++ src/main/java/Exercise4.java
@@ -2,18 +2,7 @@ public class Exercise4 {
 
   // Exchange bad word with nicer word
   public String exchangeBadword(String text) {
-    String censuredText=text;
-    if (text.contains("sgu")) {
-      censuredText = text.replaceAll("sgu", "sandelig");
-    }
-    else if (text.contains("fandeme")) {
-      censuredText = text.replaceAll("fandeme", "sandelig");
-    }
-    else if (text.contains("kraftedme")) {
-      censuredText = text.replaceAll("kraftedme", "sandelig");
-    }
-
-    return censuredText;
+    return "";
   }
 
 }
